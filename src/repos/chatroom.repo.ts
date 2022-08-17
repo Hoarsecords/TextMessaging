d58ead0cd8f5fe540c999a2519020ba15c55d7be
3ba2ii <aghonem2011@gmail.com>
2022-08-17 22:55:58 +0200

    feat: 🎸 added disconnect methodto delete connection to chatroom

--- src/repos/chatroom.repo.ts
+++ src/repos/chatroom.repo.ts
@@ -58,15 +58,52 @@ export default class ChatRoomRepo implements IRepo<ChatRoom> {
   }
 
   async connect(chatRoomId: number, user: User): RepoResult<ChatRoom> {
-    const data = await this.findById(chatRoomId);
-    const val = data.getValue();
-    if (!val) {
+    const { getResult } = await this.findById(chatRoomId);
+    const { data: chatroom, error } = getResult();
+    if (!error || !chatroom) {
       return Result.fail<RepoError>({
         code: 404,
         message: 'ChatRoom not found',
         name: 'chatroom',
       });
     }
-    return this.subscribe(val, user);
+    return this.subscribe(chatroom, user);
+  }
+
+  async disconnect(
+    chatroomId: number,
+    userId: number
+  ): RepoResult<ChatRoomUser> {
+    const connection = await ChatRoomUser.findOne({
+      where: { chatroomId, userId },
+    });
+
+    if (!connection) {
+      return Result.fail<RepoError>({
+        code: 404,
+        message: 'User is not connected to this chatroom',
+        name: 'user',
+      });
+    }
+
+    try {
+      await connection.destroy();
+      return Result.ok<ChatRoomUser>(connection);
+    } catch (error) {
+      return Result.fail<RepoError>({
+        code: 500,
+        message: "User can't be disconnected",
+        name: 'user',
+      });
+    }
+  }
+
+  async getSubscribers(chatroomId: number) {
+    const connections = await ChatRoomUser.findAll({
+      where: { chatroomId },
+      include: [User],
+    });
+
+    return connections;
   }
 }
