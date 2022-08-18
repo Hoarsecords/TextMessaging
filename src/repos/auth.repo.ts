fea7fd120f3e3a843c014ffbc60992fc83515be6
3ba2ii <aghonem2011@gmail.com>
2022-08-18 23:17:24 +0200

    feat: 🎸 added login and saveCookie methods

--- src/repos/auth.repo.ts
+++ src/repos/auth.repo.ts
@@ -1,10 +1,10 @@
-import { RepoError } from './../types/RepoError';
-import { Result } from './../types/RepoResult';
+import { Response } from 'express';
 import jwt from 'jsonwebtoken';
 import User from '../models/user';
+import { JWT_TOKEN_SECRET } from '../utils/constants';
+import { RepoError } from './../types/RepoError';
+import { Result } from './../types/RepoResult';
 import { RepoResult } from './index.types';
-
-const secret = 'secret';
 
 export type PayloadType = {
   id: number;
@@ -12,23 +12,31 @@ export type PayloadType = {
   email: string;
 };
 export default class AuthRepo {
-  static async generateJWTToken(payload: User): Promise<string> {
+  static async generateJWTToken(user: User): Promise<string> {
     const jwtExpirySeconds = 60 * 60 * 24 * 7; // 7 days
 
-    const token = jwt.sign(payload, secret, {
+    const payload: PayloadType = {
+      id: user.id,
+      username: user.username,
+      email: user.email,
+    };
+    const token = jwt.sign(payload, JWT_TOKEN_SECRET, {
       expiresIn: jwtExpirySeconds,
     });
 
     return token;
   }
   static async verifyJWTToken(token: string): Promise<boolean> {
-    const payload = (await jwt.verify(token, secret)) as PayloadType;
+    const payload = (await jwt.verify(token, JWT_TOKEN_SECRET)) as PayloadType;
 
     return payload.id !== undefined;
   }
   static async getPayload(token: string): Promise<RepoResult<PayloadType>> {
     try {
-      const payload = (await jwt.verify(token, secret)) as PayloadType;
+      const payload = (await jwt.verify(
+        token,
+        JWT_TOKEN_SECRET
+      )) as PayloadType;
       if (!payload.id || !payload.username || !payload.email)
         return Result.fail<RepoError>({
           code: 400,
@@ -44,5 +52,22 @@ export default class AuthRepo {
         name: 'token',
       });
     }
+  }
+
+  static async saveCookie(token: string, res: Response): Promise<void> {
+    res.cookie('token', token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'strict',
+      maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+    });
+  }
+
+  static async login(user: User, res: Response): Promise<string> {
+    const token = await this.generateJWTToken(user);
+
+    await this.saveCookie(token, res);
+
+    return token;
   }
 }
